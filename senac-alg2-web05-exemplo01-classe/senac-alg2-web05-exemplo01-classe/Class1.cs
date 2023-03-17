f39e9df816c1ce96d0c1b6e17f6c9216a7f17a4c
Lara Sena <laraf.sena@gmail.com>
2023-03-17 09:06:10 -0300

    Exemplo 1 - classes

---
+++ senac-alg2-web05-exemplo01-classe/senac-alg2-web05-exemplo01-classe/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senac_alg2_web05_exemplo01_classe
+{
+    class Retangulo
+    {
+        public int Base;
+        public int Altura;
+
+        public void Info()
+        {
+            Console.WriteLine($"Base {this.Base} - Altura {this.Altura}");
+        }
+
+        public int CalcularArea()
+        {
+            int area = this.Base * this.Altura;
+            return area;
+        }
+    }
+}
---
+++ senac-alg2-web05-exemplo01-classe/senac-alg2-web05-exemplo01-classe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senac_alg2_web05_exemplo01_classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r1 = new Retangulo();
+            r1.Base = 10;
+            r1.Altura = 2;
+
+            Retangulo r2 = new Retangulo();
+            r2.Base = 20;
+            r2.Altura = 4;
+
+            r1.Info();
+            r2.Info();
+
+            int a1 = r1.CalcularArea();
+            int a2 = r2.CalcularArea();
+
+            Console.WriteLine("A área do retângulo 1 é " + a1);
+            Console.WriteLine("A área do retângulo 2 é " + a2);
+            Console.ReadLine();
+        }
+
+    }
+}
